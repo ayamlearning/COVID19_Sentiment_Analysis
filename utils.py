98ca838bbb38582dd1f0f1d30c04c63eaa0136f4
Yasin Musa Ayami <ayamlearning@gmail.com>
2020-05-15 06:53:20 +0200

    notebook 2 code refactored

---
+++ utils.py
@@ -0,0 +1,94 @@
+import nltk
+import re
+import pandas as pd
+
+nltk.download('stopwords')
+sw=nltk.corpus.stopwords.words('english')
+
+def remove_stop_words(txt):
+    words= txt.split(" ")
+    words_ns=[]
+    
+    for word in words:
+        if word not in sw:
+            words_ns.append(word)
+    
+    return list_to_str(words_ns)
+
+def remove_stop_words(txt):
+    words= txt.split(" ")
+    words_ns=[]
+    
+    for word in words:
+        if word not in sw:
+            words_ns.append(word)
+    
+    return list_to_str(words_ns)
+
+
+def remove_numeric(txt):
+    words=txt.split(" ")
+    words_ns=[]
+    
+    for word in words:
+        if word.isnumeric() == False:
+            words_ns.append(word)
+    return list_to_str(words_ns)
+
+def list_to_str(lst):
+    nwstr=""
+    for i in lst:
+        nwstr+=i+" "
+        
+    return nwstr
+
+def get_nouns(txt):
+    nouns=['NNP','NN']
+    new_tagged=[]
+    
+    text = word_tokenize(txt)
+    tagged = nltk.pos_tag(text) 
+    
+    for tag in tagged:
+        if tag[1] in nouns:
+            new_tagged.append(tag[0])
+
+    return list_to_str(new_tagged)
+
+def remove_meaninglesswords(txt,meanless_words):
+    words=txt.split(" ")
+    
+    words_ns=[]
+    
+    for word in words:
+        if word not in meanless_words:
+            words_ns.append(word)
+    
+    return list_to_str(words_ns)
+
+def remove_RT(txt):
+    words=txt.split(" ")
+    words_ns=[]
+    
+    for word in words:
+        if '@' not in word:
+            words_ns.append(word)
+    
+    return list_to_str(words_ns)
+
+def merge_df_col_val(txt_col):
+    newstr=""
+    for t in txt_col.values.flatten():
+        newstr+=t.lower() + ''
+    return newstr
+
+def remove_punct_marks(txt):
+    lst= re.findall('\w+',txt)
+    
+    return list_to_str(lst)
+
+
+def get_tweet_for_specific_range(str_strt_date,str_end_date,df):
+    strt=pd.to_datetime(str_strt_date).date()
+    end=pd.to_datetime(str_end_date).date()
+    return df[(df["date"]>=strt) & (df["date"]<=end)]
